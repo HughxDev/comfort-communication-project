5d3a5dddae4baf940fc4b82a08871577a007dc32
Hugh Guiney <hugh@hughguiney.com>
2017-06-27 17:06:30 -0700

    Gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,30 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulp = require( 'gulp' );
+const vulcanize = require( 'gulp-vulcanize' );
+const babel = require( 'gulp-babel' );
+const crisper = require( 'gulp-crisper' );
+
+/*
+gulp.task('default', () => {
+    return gulp.src('src/app.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+});
+*/
+
+gulp.task( 'default', function () {
+  return gulp.src( 'src/**/*.html' )
+    .pipe( vulcanize( {
+      abspath: '',
+      excludes: [],
+      stripExcludes: false,
+      inlineScripts: false
+    } ) )
+    .pipe( crisper( {
+      scriptInHead: false, // true is default 
+      onlySplit: false
+    } ) )
+    .pipe( gulp.dest( 'dest' ) )
+  ;
+} );

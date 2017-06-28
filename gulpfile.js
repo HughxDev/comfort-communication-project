a538c113f1469e70e87de14f5abf50bc8d7945bc
Hugh Guiney <hugh@hughguiney.com>
2017-06-27 17:15:20 -0700

    Gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require( 'gulp' );
 const vulcanize = require( 'gulp-vulcanize' );
 const babel = require( 'gulp-babel' );
 const crisper = require( 'gulp-crisper' );
+const del = require( 'del' );
 
 /*
 gulp.task('default', () => {
@@ -14,15 +15,17 @@ gulp.task('default', () => {
 */
 
 gulp.task( 'default', function () {
+  del( [ 'dest' ] );
+
   return gulp.src( 'src/**/*.html' )
-    .pipe( vulcanize( {
-      abspath: '',
-      excludes: [],
-      stripExcludes: false,
-      inlineScripts: false
-    } ) )
+    // .pipe( vulcanize( {
+    //   abspath: '',
+    //   excludes: [],
+    //   stripExcludes: false,
+    //   inlineScripts: false
+    // } ) )
     .pipe( crisper( {
-      scriptInHead: false, // true is default 
+      scriptInHead: true, // true is default 
       onlySplit: false
     } ) )
     .pipe( gulp.dest( 'dest' ) )

d2190d47362d46365fdaaaa0de4f7efb50742aaf
Hugh Guiney <hugh@hughguiney.com>
2017-06-28 19:23:00 -0700

    Gulpfile troubleshooting

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task( 'copy:server', [ 'clean' ], () => {
 
 gulp.task( 'copy:node', [ 'copy:server' ], () => {
   return gulp.src( 'node_modules/**/*' )
-    .pipe( debug( { "title": "copy:node" } ) )
+    // .pipe( debug( { "title": "copy:node" } ) )
     .pipe( gulp.dest( 'build/node_modules/' ) )
   ;
 } );
@@ -45,12 +45,12 @@ gulp.task( 'copy:app', [ 'copy:images' ], () => {
 
 gulp.task( 'copy', [ 'copy:app' ] );
 
-gulp.task( 'split', [ 'copy' ], () => {
+gulp.task( 'split:src', [ 'copy' ], () => {
   // del( [ 'split' ] );
 
   return gulp.src( 'build/src/**/*.html' )
     // .pipe( vulcanize( {} ) )
-    .pipe( debug( { "title": "split" } ) )
+    // .pipe( debug( { "title": "split" } ) )
     .pipe( crisper( {
       scriptInHead: true, // true is default 
       onlySplit: false
@@ -59,36 +59,52 @@ gulp.task( 'split', [ 'copy' ], () => {
   ;
 } );
 
-gulp.task( 'babel:bower', [ 'split' ], () => {
-  return gulp.src( 'build/bower_components/**/*.js' )
-    // .pipe( debug( { "title": "babel:bower" } ) )
-    .pipe( babel( {
-      presets: [ 'es2015-nostrict' ]
+gulp.task( 'split:bower', [ 'split:src' ], () => {
+  // del( [ 'split' ] );
+
+  return gulp.src( 'bower_components/**/*.html' )
+    // .pipe( vulcanize( {} ) )
+    // .pipe( debug( { "title": "split:bower" } ) )
+    .pipe( crisper( {
+      scriptInHead: true, // true is default 
+      onlySplit: false
     } ) )
-    .pipe( gulp.dest( 'build/bower_components/' ) );
+    .pipe( gulp.dest( 'bower_components/' ) )
   ;
 } );
 
-gulp.task( 'babel:x10', [ 'babel:bower' ], () => {
-  return gulp.src( 'build/node_modules/x10.js/**/*.js' )
-    .pipe( debug( { "title": "babel:x10" } ) )
-    .pipe( babel( {
-      presets: [ 'es2015-nostrict' ]
-    } ) )
-    .pipe( gulp.dest( 'build/node_modules/x10.js/' ) );
-  ;
-} );
+// gulp.task( 'split:polymer', [ 'split:src' ], );
 
-gulp.task( 'babel:defiant', [ 'babel:x10' ], () => {
-  return gulp.src( 'build/node_modules/defiant/dist/**/*.js' )
-    .pipe( babel( {
-      presets: [ 'es2015-nostrict' ]
-    } ) )
-    .pipe( gulp.dest( 'build/node_modules/defiant/dist/' ) );
-  ;
-} );
+// gulp.task( 'babel:bower', [ 'split' ], () => {
+//   return gulp.src( 'build/bower_components/**/*.js' )
+//     // .pipe( debug( { "title": "babel:bower" } ) )
+//     .pipe( babel( {
+//       presets: [ 'es2015-nostrict' ]
+//     } ) )
+//     .pipe( gulp.dest( 'build/bower_components/' ) );
+//   ;
+// } );
 
-gulp.task( 'babel', [ 'babel:defiant' ], () => {
+// gulp.task( 'babel:x10', [ 'babel:bower' ], () => {
+//   return gulp.src( 'build/node_modules/x10.js/**/*.js' )
+//     .pipe( debug( { "title": "babel:x10" } ) )
+//     .pipe( babel( {
+//       presets: [ 'es2015-nostrict' ]
+//     } ) )
+//     .pipe( gulp.dest( 'build/node_modules/x10.js/' ) );
+//   ;
+// } );
+
+// gulp.task( 'babel:defiant', [ 'babel:bower' ], () => {
+//   return gulp.src( 'build/node_modules/defiant/dist/**/*.js' )
+//     .pipe( babel( {
+//       presets: [ 'es2015-nostrict' ]
+//     } ) )
+//     .pipe( gulp.dest( 'build/node_modules/defiant/dist/' ) );
+//   ;
+// } );
+
+gulp.task( 'babel:src', [ 'split:src' ], () => {
   // del( [ 'build' ] );
 
   return gulp.src( 'build/src/**/*.js' )
@@ -98,5 +114,18 @@ gulp.task( 'babel', [ 'babel:defiant' ], () => {
     .pipe( gulp.dest( 'build/src/' ) );
   ;
 } );
+
+gulp.task( 'babel:bower', [ 'split:bower' ], () => {
+  // del( [ 'build' ] );
+
+  return gulp.src( [ 'bower_components/**/*.js', '!bower_components/web-component-tester/**/*.js' ] )
+    .pipe( babel( {
+      presets: [ 'es2015-nostrict' ]
+    } ) )
+    .pipe( gulp.dest( 'bower_components/' ) );
+  ;
+} );
+
+gulp.task( 'babel', [ 'babel:src', 'babel:bower' ] );
 
 gulp.task( 'default', [ 'babel' ] );
